f04f8c368671793d6e09146e48a3ccd42cf79039
Miguel Nunez <miguelnunez@MacBook-Pro-Miguel.local>
2021-04-30 16:18:36 -0500

    second commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import setuptools
+
+
+setup(
+    name='recstersp',  # Temporary package name
+    version='1.0.0',
+    description='Recommender Systems using Temporal Restricted Sequential Patterns',
+    url='',
+    author='Hilda Ana Samame Jimenez',
+    author_email='hsamame@pucp.edu.pe',
+    maintainer=['Hilda Ana Samame Jimenez', 'Yoshitomi Eduardo Maehara Aliaga'],
+    maintainer_email=['hsamame@pucp.edu.pe', 'ye.maeharaa@up.edu.pe'],
+    packages=setuptools.find_packages(),
+    license="MIT",
+    install_requires=['pandas', 'numpy', 'ast', 'psutil', 'IPython'],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires='>=2.7'
+)
